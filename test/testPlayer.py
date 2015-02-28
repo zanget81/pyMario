05137493eba93281d37ea3666135284ffb2cc4ce
angel <acampo@libertyglobal.com>
2015-02-28 18:11:15 +0100

    Adding more tests.

--- test/testPlayer.py
+++ test/testPlayer.py
@@ -7,3 +7,24 @@ class Test(unittest.TestCase):
     def testCreatePlayer(self):
         player1 = Player(Constants.PLAYER, Player.PLAYER1)
         self.assertIsNotNone(player1)
+
+    def testUpdatePlayerRight(self):
+        player1 = Player(Constants.PLAYER, Player.PLAYER1)
+        self.assertIsNotNone(player1)
+        player1.update(Constants.RIGHT)
+
+    def testUpdatePlayerLeft(self):
+        player1 = Player(Constants.PLAYER, Player.PLAYER1)
+        self.assertIsNotNone(player1)
+        player1.update(Constants.LEFT)
+
+
+    def testHandleKeyPlayerRight(self):
+        player1 = Player(Constants.PLAYER, Player.PLAYER1)
+        self.assertIsNotNone(player1)
+        player1.handleKey(Constants.RIGHT)
+
+    def testHandleKeyPlayerLeft(self):
+        player1 = Player(Constants.PLAYER, Player.PLAYER1)
+        self.assertIsNotNone(player1)
+        player1.handleKey(Constants.LEFT)
